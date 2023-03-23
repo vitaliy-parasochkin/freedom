412a03d78842611efc9a836b81a9376ecfd4b106
Vitaliy Parasochkin <parasochkinvitaliy@gmail.com>
2023-03-23 11:06:10 +0200

    added burger menu

--- source/js/script.js
+++ source/js/script.js
@@ -1,9 +1,9 @@
 //choose theme for site
 
 //burger
-const burgerIcon = document.querySelector(".header-icon");
+const burgerIcon = document.querySelector(".burger-icon");
 burgerIcon.addEventListener("click", () => {
     burgerIcon.classList.toggle("active");
-    document.querySelector(".header-menu").classList.toggle("active");
+    document.querySelector(".header-nav").classList.toggle("active");
     document.querySelector("body").classList.toggle("lock");
 });
